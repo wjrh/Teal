89d272b2fdfae10deb03ffb21a58600aab4eee86
Renan Dincer <renandincer@gmail.com>
2016-01-22 20:16:17 -0500

    change how environment variables are read

--- app/config.rb
+++ app/config.rb
@@ -6,17 +6,17 @@ module Teal
   # Returns an OpenStruct so you can chain methods off of `Teal.config`.
   def self.config
     OpenStruct.new \
-      :smtp_server 	=> yaml['smtp_server'],
-			:cookie_secret			 	=> yaml['cookie_secret'],
-			:old_cookie_secret		=> yaml['old_cookie_secret'],
-			:domain 				=> yaml['domain'],
-			:front_end_subdomain => yaml['front_end_subdomain'],
-			:api_subdomain => yaml['api_subdomain'],
-			:mongo_url 			=> yaml['mongo_url'],
-			:redis_url			=> yaml['redis_url'],
-			:smtp_user			=> yaml['smtp_user'],
-			:smtp_password	=> yaml['smtp_password'],
-			:login_source_email => yaml['login_source_email']
+      :smtp_server 				=> ENV['SMTP_SERVER'] ||= yaml['smtp_server'],
+			:cookie_secret		 	=> ENV['COOKIE_SECRET'] ||= yaml['cookie_secret'],
+			:old_cookie_secret	=> ENV['OLD_COOKIE_SECRET'] ||= yaml['old_cookie_secret'],
+			:domain 						=> ENV['DOMAIN'] ||= yaml['domain'],
+			:front_end_subdomain=> ENV['FRONT_END_SUBDOMAIN'] ||= yaml['front_end_subdomain'],
+			:api_subdomain 			=> ENV['API_SUBDOMAIN'] ||= yaml['api_subdomain'],
+			:mongo_url 			=> ENV['MONGO_URL'] ||= yaml['mongo_url'],
+			:redis_url			=> ENV['REDIS_URL'] ||= yaml['redis_url'],
+			:smtp_user			=> ENV['SMTP_USER'] ||= yaml['smtp_user'],
+			:smtp_password	=> ENV['SMTP_PASSWORD'] ||= yaml['smtp_password'],
+			:login_source_email => ENV['LOGIN_SOURCE_EMAIL'] ||= yaml['login_source_email']
   end
 
 
